2db6de150f6a76d7f8db7b4e20433bf9abcd94e4
Evan Rutten <contact@evanrutten.com>
2024-11-24 17:45:40 -0500

    Working BLE

--- src/main-master.cpp
+++ src/main-master.cpp
@@ -1,23 +1,66 @@
 #include <Arduino.h>
 #include <BLEDevice.h>
 
-#define SERVICE_UUID BLEUUID("75f0a01e-af65-4099-b8ba-2af4e66fc482")
-#define CHARACTERISTIC_UUID BLEUUID("9f4ae00e-c22a-4595-a7fd-f4176d084213")
+BLEUUID SERVICE_UUID("75f0a01e-af65-4099-b8ba-2af4e66fc482");
+BLEUUID CHARACTERISTIC_UUID_1("9f4ae00e-c22a-4595-a7fd-f4176d084213");
+BLEUUID CHARACTERISTIC_UUID_2("e373f7eb-8b75-40ce-be1c-29fae2db6725");
+BLEUUID CHARACTERISTIC_UUID_3("08b109db-00f7-46a6-9c25-e6c6d0ec7ce6");
 
-class MyAdvertisedCallbacks : public BLEAdvertisedDeviceCallbacks {
+BLEAdvertisedDevice *myDevice;
+bool doConnect = false;
+
+void notifyCallback(BLERemoteCharacteristic *pCharacteristic, uint8_t *pData,
+                    size_t length, bool isNotify) {
+  for (size_t i = 0; i < length; i++) {
+    Serial.print((char)pData[i]);
+  }
+  Serial.println();
+}
+
+void connectToServer() {
+  Serial.println(myDevice->getAddress().toString().c_str());
+
+  BLEClient *pClient = BLEDevice::createClient();
+  pClient->connect(myDevice);
+  pClient->setMTU(64);
+
+  BLERemoteService *pRemoteService = pClient->getService(SERVICE_UUID);
+  if (pRemoteService == nullptr) {
+    Serial.println("Failed to find service");
+    return;
+  }
+
+  BLERemoteCharacteristic *pCharacteristic1 =
+      pRemoteService->getCharacteristic(CHARACTERISTIC_UUID_1);
+  if (pCharacteristic1 != nullptr) {
+    pCharacteristic1->registerForNotify(notifyCallback);
+  }
+
+  BLERemoteCharacteristic *pCharacteristic2 =
+      pRemoteService->getCharacteristic(CHARACTERISTIC_UUID_2);
+  if (pCharacteristic2 != nullptr) {
+    pCharacteristic2->registerForNotify(notifyCallback);
+  }
+
+  BLERemoteCharacteristic *pCharacteristic3 =
+      pRemoteService->getCharacteristic(CHARACTERISTIC_UUID_3);
+  if (pCharacteristic3 != nullptr) {
+    pCharacteristic3->registerForNotify(notifyCallback);
+  }
+
+  Serial.println("Connected to server");
+}
+
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
   void onResult(BLEAdvertisedDevice advertisedDevice) {
     if (advertisedDevice.haveServiceUUID() &&
         advertisedDevice.isAdvertisingService(SERVICE_UUID)) {
-      BLEClient *pClient = BLEDevice::createClient();
-      pClient->connect(&advertisedDevice);
-      BLERemoteService *pRemoteService = pClient->getService(SERVICE_UUID);
-      if (pRemoteService != nullptr) {
-        BLERemoteCharacteristic *pRemoteCharacteristic =
-            pRemoteService->getCharacteristic(CHARACTERISTIC_UUID);
-        if (pRemoteService != nullptr) {
-          Serial.println(pRemoteCharacteristic->readValue().c_str());
-        }
-      }
+
+      BLEDevice::getScan()->stop();
+
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+
+      doConnect = true;
     }
   }
 };
@@ -26,11 +69,18 @@ void setup() {
   Serial.begin(115200);
 
   BLEDevice::init("Master-Controller");
-  BLEScan *pBLEScan = BLEDevice::getScan();
 
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedCallbacks());
+  BLEScan *pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
   pBLEScan->start(30);
 }
 
-void loop() { delay(1000); }
+void loop() {
+  if (doConnect) {
+    connectToServer();
+    doConnect = false;
+  }
+
+  delay(1000);
+}
--- src/main-node.cpp
+++ src/main-node.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
 
 #define VOLTAGE_PIN_1 34
 #define VOLTAGE_PIN_2 25
@@ -6,7 +7,32 @@
 
 #define TEMP_PIN 2
 
-void setup() { Serial.begin(115200); }
+#define SERVICE_UUID "75f0a01e-af65-4099-b8ba-2af4e66fc482"
+#define CHARACTERISTIC_UUID_1 "9f4ae00e-c22a-4595-a7fd-f4176d084213"
+#define CHARACTERISTIC_UUID_2 "e373f7eb-8b75-40ce-be1c-29fae2db6725"
+#define CHARACTERISTIC_UUID_3 "08b109db-00f7-46a6-9c25-e6c6d0ec7ce6"
+
+BLECharacteristic *pCharacteristic;
+
+void setup() {
+  Serial.begin(115200);
+
+  BLEDevice::init("Node-1");
+
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+      CHARACTERISTIC_UUID_1,
+      BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->start();
+}
 
 void loop() {
   int adc1 = analogReadMilliVolts(VOLTAGE_PIN_1);
@@ -21,9 +47,11 @@ void loop() {
   float pack2 = v2 - pack1;
   float pack3 = v3 - (pack1 + pack2);
 
-  Serial.printf("adc1: %d, v1: %.2fV, pack1: %.2fV\n", adc1, v1, pack1);
-  Serial.printf("adc2: %d, v2: %.2fV, pack2: %.2fV\n", adc2, v2, pack2);
-  Serial.printf("adc3: %d, v3: %.2fV, pack3: %.2fV\n", adc3, v3, pack3);
+  /*
+    Serial.printf("adc1: %d, v1: %.2fV, pack1: %.2fV\n", adc1, v1, pack1);
+    Serial.printf("adc2: %d, v2: %.2fV, pack2: %.2fV\n", adc2, v2, pack2);
+    Serial.printf("adc3: %d, v3: %.2fV, pack3: %.2fV\n", adc3, v3, pack3);
+  */
 
   int temp_adc = analogReadMilliVolts(TEMP_PIN);
 
@@ -33,8 +61,16 @@ void loop() {
   float temp =
       1.0 / (1.0 / 298.15 + (1 / 3435.0) * log(r_therm / 10.0)) - 273.15;
 
-  Serial.printf("\ntemp_adc: %d, r_therm: %0.2fkΩ, temp: %0.2f°C\n\n", temp_adc,
-                r_therm, temp);
+  // Serial.printf("\ntemp_adc: %d, r_therm: %0.2fkΩ, temp: %0.2f°C\n\n",
+  // temp_adc, r_therm, temp);
+
+  String value = "N1,V1:" + String(pack1) + ",V2:" + String(pack2) +
+                 ",V3:" + String(pack3) + ",T:" + String(temp);
+
+  pCharacteristic->setValue(value.c_str());
+  pCharacteristic->notify();
+
+  Serial.println(value);
 
   delay(1000);
 }
